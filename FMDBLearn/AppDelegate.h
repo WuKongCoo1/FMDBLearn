6be137458de3cad2e46f3f9e2cec40da7a86726a
WuKongCoo1 <wukong@kewudemac-mini.local>
2016-05-04 10:03:26 +0800

    Initial Commit

---
+++ FMDBLearn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FMDBLearn
+//
+//  Created by 吴珂 on 16/5/4.
+//  Copyright © 2016年 吴珂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
